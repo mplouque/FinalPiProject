8cb784d3bc69a83252151c4848bfa349dd8ca043
Pablo Johnson <paulj.96@gmail.com>
2016-04-29 23:16:24 +0000

    added test gpio code

---
+++ GPIOTest.py
@@ -0,0 +1,46 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+# initialize the output pins
+pi0 = 17
+pi1 = 22
+
+GPIO.setmode(GPIO.BCM)
+
+# setup the pins as output pins
+GPIO.setup(pi0, GPIO.OUT)
+GPIO.setup(pi1, GPIO.OUT)
+
+state0 = 0
+state1 = 1
+# turn on pi 1
+GPIO.output(pi0, state0)
+GPIO.output(pi1, state1)
+sleep(0.5)
+state0 = 1 - state0
+state1 = 1 - state1
+
+
+# give control to pi0
+GPIO.output(pi0, state0)
+GPIO.output(pi1, state1)
+sleep(0.5)
+state0 = 1 - state0
+state1 = 1 - state1
+
+# give control to pi1
+GPIO.output(pi0, state0)
+GPIO.output(pi1, state1)
+sleep(0.5)
+state0 = 1 - state0
+state1 = 1 - state1
+
+# give control to pi0
+GPIO.output(pi0, state0)
+GPIO.output(pi1, state1)
+sleep(0.5)
+state0 = 1 - state0
+state1 = 1 - state1
+
+
+#GPIO.cleanup()
