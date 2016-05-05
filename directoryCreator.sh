8427fed262a6460d15eb3ff843faf922b8a23b9a
Pablo Johnson <paulj.96@gmail.com>
2016-05-04 22:55:28 -0500

    actually did it right this time

--- directoryCreator.sh
+++ directoryCreator.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-for i in $(cat PokemonList.txt| grep '\n')
+for i in $(cat PokemonList.txt)
 	do
+		echo $i
 		q=$(echo $i|tr -d '\r')
+		#echo $q
 		mkdir /home/Kanto/Pokemon/$q
+		#mkdir /home/abkmp/testing/$q
 		#rm -r /home/abkmp/dirTest/$i
 		done
 
